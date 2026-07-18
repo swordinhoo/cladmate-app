5d7d6e29d66a2539fa7162bb5fb408d4bec36cbb
Craig <craig@cladmate.app>
2026-07-18 16:10:43 +0000

    v1.0.3: seam picker under preview

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'cladmate-4e06f75e';
+const CACHE = 'cladmate-e0b73d06';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting())); });
 self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim())); });
