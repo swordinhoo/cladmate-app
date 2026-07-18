f88c14279f9d6c474a26cbc9c3333728d0040f87
Craig <craig@cladmate.app>
2026-07-18 22:41:00 +0000

    v1.5: double bend

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'cladmate-97eaa3cb';
+const CACHE = 'cladmate-95ede444';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting())); });
 self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim())); });
