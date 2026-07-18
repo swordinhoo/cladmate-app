446dc0d8afe45e95cfdace3b117f29237d2fbcc6
Craig <craig@cladmate.app>
2026-07-18 15:39:27 +0000

    v1.0.2: home reorder

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'cladmate-852c0ce6';
+const CACHE = 'cladmate-4e06f75e';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting())); });
 self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim())); });
