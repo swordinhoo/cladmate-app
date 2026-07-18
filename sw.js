9eb830e14917bb441e096c9daad83d53d333a64a
Craig <craig@cladmate.app>
2026-07-18 19:55:43 +0000

    v1.3: trunnion on bend

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'cladmate-13ea2962';
+const CACHE = 'cladmate-4324307f';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting())); });
 self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim())); });
